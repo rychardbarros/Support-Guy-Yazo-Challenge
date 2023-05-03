e6268f1a218415400a676e2d776d53293ba5dfdb
Max Milan <max.oliveira@yazo.com.br>
2023-05-03 10:15:11 -0300

    chore: merge

--- FrontEnd/src/shared/molecules/ChallengeCard/ChallengeCard.tsx
+++ FrontEnd/src/shared/molecules/ChallengeCard/ChallengeCard.tsx
@@ -1,16 +1,16 @@
-import { Box, Heading, LinkBox, LinkOverlay, Text } from '@chakra-ui/react'
+import { Box, Heading, LinkBox, LinkOverlay, Text } from '@chakra-ui/react';
 
 export function ChallengeCard({ titleChallenge, descriptionChallenge }) {
-	return (
-		<LinkBox as="article" maxW="sm" p="5" borderWidth="1px" rounded="md">
-			<Box as="time" fontWeight="bold" color="blue.300" dateTime="2021-01-15 15:30:00 +0000 UTC">
-				{titleChallenge}
-			</Box>
+  return (
+    <LinkBox as="article" maxW="sm" p="5" borderWidth="1px" rounded="md">
+      <Box as="time" fontWeight="bold" color="blue.300" dateTime="2021-01-15 15:30:00 +0000 UTC">
+        {titleChallenge}
+      </Box>
 
-			<Text mb="3">{descriptionChallenge}</Text>
-			<Box as="a" color="teal.400" href="#" fontWeight="bold">
+      <Text mb="3">{descriptionChallenge}</Text>
+      <Box as="a" color="teal.400" href="#" fontWeight="bold">
         Some inner link
-			</Box>
-		</LinkBox>
-	)
+      </Box>
+    </LinkBox>
+  );
 }
